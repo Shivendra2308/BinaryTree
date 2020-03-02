9e69d4320c0580e4ceaaf07396b1ffb038d51b98
Shivendra2308 <shivendra.singh_cs18@gla.ac.in>
2020-03-02 17:30:25 +0530

    main class executed

---
+++ src/MyBSTmain.java
@@ -0,0 +1,11 @@
+import myBST.MyBST;
+
+public class MyBSTmain {
+    public static void main(String args[]) {
+        MyBST obj = new MyBST();
+        obj.insert(10);
+        obj.insert(25);
+        obj.insert(21);
+        obj.insert(12);
+    }
+}
